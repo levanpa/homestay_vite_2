0b075a5bbfa356a8eec99d38b6a74ad43122f458
Kai Pa <levanpa00@gmail.com>
2022-05-19 19:26:43 +0700

    code 1905

--- main.js
+++ main.js
@@ -1,5 +1,40 @@
 // import '/src/style.sass'
 
+function getData() {
+  axios.get('http://localhost:1337/api/homestays/1')
+    .then(function (response) {
+      let data = response.data.data
+      // console.log('success', JSON.parse(data.attributes.facility))
+      renderDataPageDetail(data.attributes)
+    })
+    .catch(function (error) {
+      console.log('error', error)
+    })
+}
+
+function renderDataPageDetail(item) {
+  let article = $('.article-product-detail')
+  if (!article.length) return
+  $('.product-name').text(item.name)
+  $('.name-alt').text(item.name_en)
+  $('.address address').text(item.address)
+  $('.js-product-images .image').attr('src', item.images)
+  $('.product-price').text(item.price)
+  $('.product-description').text(item.description)
+  $('.utility-wrapper .product-rooms span').text(item.rooms)
+  $('.utility-wrapper .product-surface span').text(`${item.surface}m2`)
+  // basic_info
+  let basicInfo = JSON.parse(item.basic_info)
+  for (let key in basicInfo) {
+    console.log(`${key}: ${basicInfo[key]}`);
+    $('.utility-wrapper .basic-info').append(`<li><p>${key}</p><span>${basicInfo[key]}</span></li>`)
+  }
+  $('.address').text(item.address)
+  $('.address').text(item.address)
+  $('.address').text(item.address)
+
+}
+
 function calcScoreBar() {
   let bar = $('[class~="js-bar"]')
   bar.each((index, item) => {
@@ -18,7 +53,21 @@ function slider() {
   let productSlider01 = $('.js-slider-01')
   if (productSlider01.length) {
     productSlider01.slick({
-      slidesToShow: 6
+      slidesToShow: 6,
+      responsive: [
+        {
+          breakpoint: 769,
+          settings: {
+            slidesToShow: 3,
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+          }
+        },
+      ]
     })
   }
 }
@@ -51,23 +100,23 @@ function handleNotices() {
 }
 
 function browserCheck() {
-  var html = $('html');
-  var userAgent = navigator.userAgent.toLowerCase();
+  var html = $('html')
+  var userAgent = navigator.userAgent.toLowerCase()
   if (navigator.platform.toLowerCase().indexOf('mac') >= 0) {
-    html.addClass('is-mac');
+    html.addClass('is-mac')
   }
   if (userAgent.indexOf('firefox') > -1) {
-    html.addClass('is-firefox');
+    html.addClass('is-firefox')
   }
   if (userAgent.indexOf('safari') != -1) {
     if (userAgent.indexOf('chrome') > -1) {
-      html.addClass('is-chrome');
+      html.addClass('is-chrome')
     } else {
-      html.addClass('is-safari');
+      html.addClass('is-safari')
     }
   }
   if (userAgent.indexOf('android') > -1) {
-    html.addClass('is-android');
+    html.addClass('is-android')
   }
 }
 
@@ -77,4 +126,5 @@ $(function () {
   slider()
   calcScoreBar()
   handleNotices()
+  getData()
 })
